8eea125e76c994685ea5920f1986185af66e830b
SteffenGorenflo <SteffenGorenflo@gmail.com>
2015-07-05 13:39:20 +0200

    add logging class

---
+++ src/util/logging.ts
@@ -0,0 +1,15 @@
+var server;
+
+export function initLogging(hapiServer:any) {
+    server = hapiServer;
+}
+
+
+export function log(logging:string) {
+    if (!server) {
+        console.error('Server not initialized for logging');
+        return;
+    }
+
+    server.log(logging);
+}
